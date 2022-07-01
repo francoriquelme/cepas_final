0dac862682433e43bb17e21db2152ea57bcb6916
francoriquelme <francoestebanru@gmail.com>
2022-07-01 12:54:35 -0400

    feat: validations

--- app/models/strain.rb
+++ app/models/strain.rb
@@ -1,2 +1,6 @@
 class Strain < ApplicationRecord
+    validates :name, presence: true, uniqueness: true
+    # Relacionamos la tabla con la intermedia 
+    has_many :wine_strains
+    has_many :wines, through: :wine_strains
 end
--- app/models/wine.rb
+++ app/models/wine.rb
@@ -1,2 +1,6 @@
 class Wine < ApplicationRecord
+    validates :name, presence: true, uniqueness: true
+    # Relacionamos la tabla con la intermedia 
+    has_many :wine_strains
+    has_many :strains, through: :wine_strains
 end
